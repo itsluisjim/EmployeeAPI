84a499c91d37d9c57f64c0894f6d9d2f11e38433
Luis <eternal_pitta0b@icloud.com>
2024-04-22 19:41:33 -0500

    add file

---
+++ src/test/java/com/itsluisjim/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsluisjim.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
